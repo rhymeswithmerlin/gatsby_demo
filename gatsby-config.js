aa135fea053f216c28989e9350f8765a2a44c654
Christopher Sherlin <christophersherlin@Christophers-MacBook-Pro.local>
2020-07-22 13:21:34 -0400

    Second Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sherlin`,
+      summary: `Demo Blog`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      // twitter: ``,
     },
   },
+  pathPrefix: "/gatsby-demo",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
